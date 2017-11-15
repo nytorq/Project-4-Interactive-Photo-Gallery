225d8ad6ecd26c05b9db30ee9e2a5aad39a00585
Charlie Prator <nytorq@users.noreply.github.com>
2017-11-14 20:35:39 -0700

    Final polish to meet recent feedback (#6)
    1 Made some CSS changes to fit desktop mockup
    2. Search input is now case insensitive

--- scripts/search.js
+++ scripts/search.js
@@ -2,32 +2,15 @@
 const search = () => {
   const input = document.getElementById('search_input');
   const imageArray = document.getElementsByClassName("image");
-  let filter = input.value;
+  let filter = input.value.toUpperCase();
   for (i=0; i<imageArray.length ; i++) {
-    if (imageArray[i].alt.search(filter) < 0) {
-      imageArray[i].style.display = "none";
+    imageArray[i].alt = imageArray[i].alt.toUpperCase();
+    if (input.value ==="") {
+      imageArray[i].style.opacity = "inherit";
+    } else if (imageArray[i].alt.search(filter) < 0){
+      imageArray[i].style.opacity = "0.05";
     } else {
-      imageArray[i].style.display = "inherit";
+      imageArray[i].style.opacity = "inherit";
     }
   }
 };
-
-/*
-Create a variable for input.
-Then
-Use a for loop to grab every single alt text tied to an image
-  1. Create an object literal. Empty
-  2. Enter and create new objects of all 12 pictures on the page. They should have the following
-  keys and values:
-    1. Image: 01
-    2. alt: "string of alt text"
-2. Store them in a local variable
-3. Use a for loop (or whatever kind of loop) and search within the alt text of each image for matches.
-4. If there is no match, set the display property of the <img>'s parent to none'
-
-if (imageArray[0].alt.search(filter) > -1) {imageArray[0].style.display = "none";}
-
-for (i=0; i<9; i++)
-
-*/
-//Functions
